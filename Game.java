5983220bc110f6962257e63da36c44d52c881abb
alakkur923 <alakkur923@LHS-MCMN608-13.local>
2025-05-09 12:10:48 -0700

    Updated Hash Map

--- Game.java
+++ Game.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game{
     
-  private ArrayList<Player> players;
+  private HashMap<String, Player> players;
   public Game(){
     players = null;
   }
 
   private void addPlayers(String name){
     Player p = new Player(name);
-    players.add(p);
+    players.put(name, p);
   }
+  
+
 }
